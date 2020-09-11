efaa5dcf3f972b6abd32fcf01a555b7c8582426e
Kasim Helil <kasimhelil@gmail.com>
2020-09-11 10:56:53 -0400

    switched to react-bootstrap tabs

--- src/components/tabs/Tabs.js
+++ src/components/tabs/Tabs.js
@@ -1,19 +1,23 @@
-import React from "react";
-import RRTabs from "react-responsive-tabs";
-import "react-responsive-tabs/styles.css";
+import React, { useState } from "react";
+// import RRTabs from "react-responsive-tabs";
+// import "react-responsive-tabs/styles.css";
+import { Tabs as RBTabs } from "react-bootstrap";
 import "./Tabs.css";
-
-// const tabs = [{ title: 'Settings', link: '/tools/settings' }, { title: 'Audit Log', link: '/tools/auditLog' }, { title: 'Code Editor', link: '/tools/codeEditor' }];
+import { Tab } from "react-bootstrap";
 
 const Tabs = props => {
-  const tablist = props.tabs.map((tab, index) => ({
-    title: tab.title,
-    getContent: () => tab.link,
-    key: index,
-    tabClassName: "tab",
-    panelClassName: "panel"
-  }));
-
-  return <RRTabs items={tablist} className="is-fullwidth" showInkBar={true} />;
+  const [key, setKey] = useState("Summary");
+  const tablist = props.tabs.map(tab => {
+    return (
+      <Tab eventKey={tab.title} title={tab.title} key={tab.title}>
+        {tab.link}
+      </Tab>
+    );
+  });
+  return (
+    <RBTabs activeKey={key} onSelect={k => setKey(k)}>
+      {tablist}
+    </RBTabs>
+  );
 };
 export default Tabs;
