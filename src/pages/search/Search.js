caf94eda590973932f84244aa449994616e6556b
Kasim Helil <kasimhelil@gmail.com>
2020-09-14 23:03:25 -0400

    highlight searched text in result cell

--- src/pages/search/Search.js
+++ src/pages/search/Search.js
@@ -5,45 +5,79 @@ import { search } from "../../services/serviceWrapper";
 import { hasData, localeDate } from "../../utils/utils";
 import { Container } from "react-bootstrap";
 import { useParams } from "@reach/router";
+import "./Search.scss";
 
 const Search = props => {
   const [data, setData] = useState([]);
   const { searchParam } = useParams();
   const cb = () => {};
+  const isSearchedText = text => {
+    return ("" + text).toUpperCase().includes(searchParam.toUpperCase());
+  };
   const Headers = [
     {
       Accessor: "passengerId",
-      Header: "Id"
+      Header: "Id",
+      Cell: ({ row }) => {
+        const highlight = isSearchedText(row.original.passengerId) ? "highlight" : "";
+        return <span className={highlight}>{row.original.passengerId}</span>;
+      }
     },
     {
       Accessor: "lastName",
-      Header: "Last Name"
+      Header: "Last Name",
+      Cell: ({ row }) => {
+        const highlight = isSearchedText(row.original.lastName) ? "highlight" : "";
+        return <span className={highlight}>{row.original.lastName}</span>;
+      }
     },
     {
       Accessor: "firstName",
-      Header: "First Name"
+      Header: "First Name",
+      Cell: ({ row }) => {
+        const highlight = isSearchedText(row.original.firstName) ? "highlight" : "";
+        return <span className={highlight}>{row.original.firstName}</span>;
+      }
     },
     {
       Accessor: "middleName",
-      Header: "Middle Name"
+      Header: "Middle Name",
+      Cell: ({ row }) => {
+        const highlight = isSearchedText(row.original.middleName) ? "highlight" : "";
+        return <span className={highlight}>{row.original.middleName}</span>;
+      }
     },
     {
       Accessor: "flightNumber",
-      Header: "Flight Number"
+      Header: "Flight Number",
+      Cell: ({ row }) => {
+        const highlight = isSearchedText(row.original.flightNumber) ? "highlight" : "";
+        return <span className={highlight}>{row.original.flightNumber}</span>;
+      }
     },
     {
       Accessor: "origin",
-      Header: "Origin"
+      Header: "Origin",
+      Cell: ({ row }) => {
+        const highlight = isSearchedText(row.original.origin) ? "highlight" : "";
+        return <span className={highlight}>{row.original.origin}</span>;
+      }
     },
     {
       Accessor: "etd",
       Header: "ETD",
-      Cell: ({ row }) => localeDate(row.original.eta)
+      Cell: ({ row }) => {
+        const highlight = isSearchedText(row.original.etd) ? "highlight" : "";
+        return <span className={highlight}>{localeDate(row.original.etd)}</span>;
+      }
     },
     {
       Accessor: "eta",
       Header: "ETA",
-      Cell: ({ row }) => localeDate(row.original.eta)
+      Cell: ({ row }) => {
+        const highlight = isSearchedText(row.original.eta) ? "highlight" : "";
+        return <span className={highlight}>{localeDate(row.original.eta)}</span>;
+      }
     }
   ];
 
