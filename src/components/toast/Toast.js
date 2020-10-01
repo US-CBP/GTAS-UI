edff72718b21d997cfb16f78cd145f364a6fdc32
Kasim Helil <kasimhelil@gmail.com>
2020-10-01 10:21:36 -0400

    Toast component that mimics push notification

---
+++ src/components/toast/Toast.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Toast as RBToast } from "react-bootstrap";
+import PropTypes from "prop-types";
+import "./Toast.scss";
+function Toast(props) {
+  const minute = 60000;
+  const TOAST_DELAY_IN_MILLI = props.delay || minute;
+  return (
+    <div className="toast-container">
+      <RBToast
+        onClose={props.onClose}
+        show={props.show}
+        delay={TOAST_DELAY_IN_MILLI}
+        autohide
+        className={props.variant}
+      >
+        <RBToast.Header>{props.header}</RBToast.Header>
+        <RBToast.Body>{props.body}</RBToast.Body>
+      </RBToast>
+    </div>
+  );
+}
+
+Toast.propTypes = {
+  onClose: PropTypes.func,
+  header: PropTypes.string,
+  body: PropTypes.string,
+  show: PropTypes.bool,
+  delay: PropTypes.number,
+  variant: PropTypes.string
+};
+
+export default Toast;
