b5d9aa23acc126b671c1c7f147398648fca65605
dave <david.j.ertel@gmail.com>
2020-10-20 11:17:02 -0400

    Updated styling for login page.

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -6,7 +6,6 @@ import { Alert, Card, Button } from "react-bootstrap";
 import { navigate, Link } from "@reach/router";
 import { UserContext } from "../../context/user/UserContext";
 import "./Login.scss";
-import Logo from "../../images/WCO_GTAS_logo.svg";
 
 const Login = () => {
   const ctx = useContext(UserContext);
@@ -39,57 +38,70 @@ const Login = () => {
   };
 
   return (
-    <div className="login-page-container">
-      <Card className="transparent-white-card">
-        <Card.Img variant="top" src={Logo} className="logo" />
-        <div className="placeholder"></div>
-        <Card.Body className="login-card-body">
-          <Link to="/forgot-password">Forgot my password</Link>
-          <br />
-          <Form
-            title=""
-            submitText="LOGIN"
-            submitService={login.post}
-            callback={loginHandler}
-            id="loginform"
-          >
-            <LabelledInput
-              inputType="text"
-              alt="Enter the user name"
-              name="username"
-              labelText=""
-              placeholder="Username"
-              datafield="username"
-              required="required"
-              inputVal=""
-              autofocus="true"
-              className="login-labeled-input"
-            />
-            <LabelledInput
-              inputType="password"
-              alt="Enter the password"
-              name="password"
-              labelText=""
-              placeholder="Password"
-              datafield="password"
-              required="required"
-              inputVal=""
-              className="login-labeled-input"
-            />
-          </Form>
-          <div>
-            {alertVis ? (
-              <Alert variant="danger" dismissible onClose={() => setAlertVis(false)}>
-                Login failed.
-              </Alert>
-            ) : (
-              <Button variant="outline-info" onClick={() => navigate("/signup")}>
-                Sign Up
-              </Button>
-            )}
+    <div className="container-fluid">
+      <div className="row no-gutter">
+        <div className="d-none d-md-flex col-md-4 col-lg-6 bg-image"></div>
+        <div className="col-md-8 col-lg-6">
+          <div className="login d-flex align-items-center py-5">
+            <div className="container">
+              <div className="row">
+                <div className="col-md-9 col-lg-8 mx-auto">
+                  <h3 className="login-heading mb-4">Welcome to GTAS!</h3>
+                  <Form
+                    title=""
+                    submitText="Login"
+                    submitService={login.post}
+                    callback={loginHandler}
+                    id="loginform"
+                  >
+                    <LabelledInput
+                      inputType="text"
+                      alt="Enter the user name"
+                      name="username"
+                      labelText=""
+                      placeholder="Username"
+                      datafield="username"
+                      required="required"
+                      inputVal=""
+                      autofocus="true"
+                      className="login-labeled-input"
+                    />
+                    <LabelledInput
+                      inputType="password"
+                      alt="Enter the password"
+                      name="password"
+                      labelText=""
+                      placeholder="Password"
+                      datafield="password"
+                      required="required"
+                      inputVal=""
+                      className="login-labeled-input"
+                    />
+                  </Form>
+                  <Link class="small" to="/forgot-password">
+                    Forgot password?
+                  </Link>
+                  <div>
+                    {alertVis ? (
+                      <Alert
+                        variant="danger"
+                        dismissible
+                        onClose={() => setAlertVis(false)}
+                      >
+                        Login failed.
+                      </Alert>
+                    ) : (
+                      <Button variant="outline-info" onClick={() => navigate("/signup")}>
+                        Sign Up
+                      </Button>
+                    )}
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
-        </Card.Body>
-      </Card>
+        </div>
+      </div>
     </div>
   );
 };
