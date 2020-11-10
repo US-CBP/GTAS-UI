ee1c55f1bfc10fd96d6e16ecc1b1bff507467d14
gtasautumn <49160279+gtasautumn@users.noreply.github.com>
2020-11-10 17:23:51 -0500

    added cancel button to modal

--- src/pages/admin/settings/settingModal.js
+++ src/pages/admin/settings/settingModal.js
@@ -34,6 +34,7 @@ const SettingModal = props => {
           action="edit"
           refreshOnSubmit
           data={props.data}
+          cancellable
         >
           <LabelledInput
             datafield
