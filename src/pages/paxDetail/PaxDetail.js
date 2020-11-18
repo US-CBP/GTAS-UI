1f7b59ddb2584d5e4746361a1d454fd10746b968
gtasautumn <49160279+gtasautumn@users.noreply.github.com>
2020-11-18 15:18:23 -0500

    paxdetail child pages at media max-width 767
    basic fixes for grid and container alignment across the large and medium viewport widths

--- src/pages/paxDetail/PaxDetail.js
+++ src/pages/paxDetail/PaxDetail.js
@@ -167,7 +167,6 @@ const PaxDetail = props => {
     );
   };
   const getFlightBadgeData = res => {
-    console.log(res);
     return {
       flightNumber: addLinkToFlight(res),
       carrier: "",
