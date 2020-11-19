f08744decdca8e24458fec9a9edb7a77ef87ff94
gtasautumn <49160279+gtasautumn@users.noreply.github.com>
2020-11-18 20:06:53 -0500

    rem log

--- src/components/tabs/Tabs.js
+++ src/components/tabs/Tabs.js
@@ -5,7 +5,6 @@ import "./Tabs.css";
 const Tabs = props => {
   const [key, setKey] = useState();
   const tablist = props.tabs.map(tab => {
-    console.log(tab);
     return (
       <Tab {...tab} eventKey={tab.titleText} key={tab.titleText}>
         {tab.link}
