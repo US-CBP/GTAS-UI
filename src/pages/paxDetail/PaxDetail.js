6d3feb0428af29b186e2fb520b089b18d07cca48
gtasautumn <49160279+gtasautumn@users.noreply.github.com>
2021-01-14 00:50:15 -0500

    license headers pages

--- src/pages/paxDetail/PaxDetail.js
+++ src/pages/paxDetail/PaxDetail.js
@@ -1,3 +1,7 @@
+// All GTAS code is Copyright 2016, The Department of Homeland Security (DHS), U.S. Customs and Border Protection (CBP).
+//
+// Please see license.txt for details.
+
 import React, { useState, useEffect, useContext } from "react";
 import { UserContext } from "../../context/user/UserContext";
 import Tabs from "../../components/tabs/Tabs";
--- src/pages/tools/queryrules/QRModal.js
+++ src/pages/tools/queryrules/QRModal.js
@@ -1,3 +1,7 @@
+// All GTAS code is Copyright 2016, The Department of Homeland Security (DHS), U.S. Customs and Border Protection (CBP).
+//
+// Please see license.txt for details.
+
 import React, { useState, useEffect, useMemo, useContext } from "react";
 import RAQB from "../../../components/raqb/RAQB";
 import { Button, Row } from "react-bootstrap";
