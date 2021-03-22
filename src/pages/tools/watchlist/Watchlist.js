aadc8a9a6219ba1cf938ed7d65647fcb28b83eb0
gtasautumn <49160279+gtasautumn@users.noreply.github.com>
2021-03-22 12:21:52 -0400

    disable filters on watchlist docs edit/delete fields

--- src/pages/tools/watchlist/Watchlist.js
+++ src/pages/tools/watchlist/Watchlist.js
@@ -274,6 +274,7 @@ const Watchlist = props => {
       Header: ["edit001", "Edit"],
       disableExport: true,
       disableSortBy: true,
+      disableFilters: true,
       Cell: ({ row }) => getEditRowData(row.original)
     },
     { Accessor: "documentType", Xl8: true, Header: ["wl011", "Document Type"] },
@@ -283,6 +284,7 @@ const Watchlist = props => {
       Accessor: "delete",
       Xl8: true,
       Header: ["wl014", "Delete"],
+      disableFilters: true,
       disableExport: true,
       Cell: ({ row }) => getDeleteColumnData(row.original.id)
     }
