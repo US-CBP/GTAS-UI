d0462620459d54cfe59aa89d973fe5248eb01fb7
Kasim Helil <kasimhelil@gmail.com>
2021-07-07 00:38:42 -0400

    Added rich text editor

--- src/components/inputs/richTextEditor/RichTextEditor.js
+++ src/components/inputs/richTextEditor/RichTextEditor.js
@@ -1,25 +1,33 @@
-import React, { useEffect, useRef, useState } from "react";
-import { Editor, EditorState } from "draft-js";
-
+import React, { useState } from "react";
+import { Editor, EditorState, convertToRaw } from "draft-js";
+import "draft-js/dist/Draft.css";
+import "./RichTextEditor.scss";
+import Toolbar from "./toolbar/Toolbar";
+import { toolbarItems } from "./constants";
 const RichTextEditor = props => {
   const [editorState, setEditorState] = useState(EditorState.createEmpty());
-  const editor = useRef(null);
-
-  const focusEditor = () => {
-    editor.current.focus();
-  };
 
   const onChange = editorState => {
+    const plainText = editorState.getCurrentContent().getPlainText("");
+    const richTextFormat = convertToRaw(editorState.getCurrentContent());
     setEditorState(editorState);
+    props.callback({ plainText: plainText, richTextFormat: richTextFormat });
   };
 
-  useEffect(() => {
-    focusEditor();
-  }, []);
-
   return (
-    <div onClick={focusEditor}>
-      <Editor ref={editor} editorState={editorState} onChange={onChange} />
+    <div className={`${props.className} rte-container`}>
+      <Toolbar
+        toolbarItems={toolbarItems}
+        editorState={editorState}
+        onChange={onChange}
+      />
+      <Editor
+        editorState={editorState}
+        onChange={onChange}
+        placeholder={props.placeholder}
+        readOnly={props.readOnly}
+        className="rte-editor"
+      />
     </div>
   );
 };
